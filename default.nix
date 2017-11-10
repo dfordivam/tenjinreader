d4800b8dc0d12fd8c16e011822ce08515b029583
Divam <dfordivam@gmail.com>
2017-11-10 11:12:39 +0900

    Add yesod-auth-outh2 package dependency

--- default.nix
+++ default.nix
@@ -17,4 +17,13 @@
     ghc = ["common" "backend" "frontend"];
     ghcjs = ["common" "frontend"];
   };
+
+  overrides = self: super: {
+    yesod-auth-oauth2 = pkgs.haskell.lib.doJailbreak (self.callCabal2nix "yesod-auth-oauth2" (pkgs.fetchFromGitHub {
+      owner = "thoughtbot";
+      repo = "yesod-auth-oauth2";
+      rev = "937ad572a31dcbb2d5fe75b91f689c7a2f6ecfca";
+      sha256 = "0ai4axdh5x87qrkzn6a9i0a2srs6gkjbbiywihrv7ydmh2caqjya";
+    }) {});
+  };
 })
