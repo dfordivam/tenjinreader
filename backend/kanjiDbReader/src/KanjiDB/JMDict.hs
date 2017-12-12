0a93140f15266cf34590fd947bc333276f41a2a5
Divam <dfordivam@gmail.com>
2017-12-12 16:46:06 +0900

    Move the makeFurigana api

--- backend/kanjiDbReader/src/KanjiDB/JMDict.hs
+++ backend/kanjiDbReader/src/KanjiDB/JMDict.hs
@@ -4,8 +4,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module KanjiDB.JMDict
-  (getJMDictEntries
-  , makeFurigana)
+  (getJMDictEntries)
   where
 
 import Protolude hiding (to)
@@ -120,64 +119,3 @@ makeVocab e = case e ^. entryKanjiElements of
 
 getMecab =  new ["mecab", "-d"
       , "/home/divam/nobup/mecab-tools/mecab-ipadic-neologd-output-files"]
-
-makeFurigana :: KanjiPhrase -> ReadingPhrase -> Either Text Vocab
-makeFurigana (KanjiPhrase k) (ReadingPhrase r) = Vocab
-  <$> (g (map katakanaToHiragana kgs) (katakanaToHiragana r))
-  where
-    g kgs r = case reverse kgs of
-      (kl:krev) -> case T.stripSuffix kl r of
-        (Just prfx) -> (\x -> x ++ [Kana kl]) <$> f (reverse krev) prfx
-        Nothing -> f kgs r
-
-    kgs = T.groupBy (\ a b -> (isKana a) == (isKana b)) k
-    f :: [Text] -> Text -> Either Text [KanjiOrKana]
-    f [] r
-      | T.null r = Right []
-      | otherwise = Right [Kana r]
-
-    f (kg:[]) r
-      | T.null r = Left "Found kg, but r is T.null"
-      | otherwise = if kg == r
-        then Right [Kana r]
-        else if (isKana (T.head kg))
-          then Left $ "Found kana not equal to r: " <> kg <> ", " <> r
-          else Right [KanjiWithReading (Kanji kg) r]
-
-    f (kg:kg2:kgs) r
-      | T.null r = Left "r is null"
-      | otherwise = if (isKana (T.head kg))
-        then case (T.stripPrefix kg r) of
-          (Just rs) -> ((Kana kg) :) <$> (f (kg2:kgs) rs)
-          Nothing -> Left $ "stripPrefix: " <> kg <> ", " <> r
-        else case (T.breakOn kg2 (T.tail r)) of
-          (rk, rs)
-            -> (KanjiWithReading (Kanji kg) (T.cons (T.head r) rk) :) <$> (f (kg2:kgs) rs)
-
-testMakeFurigana = map (\(a,b) -> makeFurigana (KanjiPhrase a) (ReadingPhrase b))
-  [("いじり回す", "いじりまわす")
-  ,("弄りまわす", "いじりまわす")
-  , ("弄り回す", "いじりまわす")
-  , ("いじり回す", "いじりまわ") -- Fail
-  , ("窺う", "うかがう")
-  , ("黄色い", "きいろい")
-  , ("額が少ない", "がくがすくない")
-  -- , ("霞ヶ関", "かすみがせき")  -- Reading with no kanji
-  -- , ("霞ケ関", "かすみがせき")  -- Reading with no kanji
-  , ("ケント紙", "ケントし")
-  , ("二酸化ケイ素", "にさんかケイそ")
-  , ("ページ違反", "ぺーじいはん")
-  , ("シェリー酒", "シェリーしゅ")
-  ]
-
--- isSameAs t1 t2
---   | T.length t1 == T.length t2 = all compareChars (zip (T.unpack t1) (T.unpack t2))
---   | otherwise = False
-
--- isSameAsC c1 c2 = compareChars (c1, c2)
-
--- compareChars = f
---   where
---     f ('ヶ', c2) = elem c2 ['か', 'が','ヶ', 'ケ']
---     f ('ケ', c2) = elem c2 ['か', 'が','ヶ', 'ケ']
---     f (c1, c2) = c1 == c2
--- common/src/Common.hs
+++ common/src/Common.hs
@@ -23,6 +23,8 @@ import Data.BTree.Primitives (Value)
 import Data.JMDict.AST
 import Data.List.NonEmpty (NonEmpty(..))
 import DerivingInstances ()
+import qualified Data.Text as T
+import NLP.Japanese.Utils
 
 instance Value Int
 instance Value a => Value (Maybe a)
@@ -172,6 +174,68 @@ data ReviewType =
 
 type AnnotatedText = [(Either Text (Vocab, [VocabId], Bool))]
 
+-- APIs -- may be move from here
+
+makeFurigana :: KanjiPhrase -> ReadingPhrase -> Either Text Vocab
+makeFurigana (KanjiPhrase k) (ReadingPhrase r) = Vocab
+  <$> (g (map katakanaToHiragana kgs) (katakanaToHiragana r))
+  where
+    g kgs r = case reverse kgs of
+      (kl:krev) -> case T.stripSuffix kl r of
+        (Just prfx) -> (\x -> x ++ [Kana kl]) <$> f (reverse krev) prfx
+        Nothing -> f kgs r
+
+    kgs = T.groupBy (\ a b -> (isKana a) == (isKana b)) k
+    f :: [Text] -> Text -> Either Text [KanjiOrKana]
+    f [] r
+      | T.null r = Right []
+      | otherwise = Right [Kana r]
+
+    f (kg:[]) r
+      | T.null r = Left "Found kg, but r is T.null"
+      | otherwise = if kg == r
+        then Right [Kana r]
+        else if (isKana (T.head kg))
+          then Left $ "Found kana not equal to r: " <> kg <> ", " <> r
+          else Right [KanjiWithReading (Kanji kg) r]
+
+    f (kg:kg2:kgs) r
+      | T.null r = Left "r is null"
+      | otherwise = if (isKana (T.head kg))
+        then case (T.stripPrefix kg r) of
+          (Just rs) -> ((Kana kg) :) <$> (f (kg2:kgs) rs)
+          Nothing -> Left $ "stripPrefix: " <> kg <> ", " <> r
+        else case (T.breakOn kg2 (T.tail r)) of
+          (rk, rs)
+            -> (KanjiWithReading (Kanji kg) (T.cons (T.head r) rk) :) <$> (f (kg2:kgs) rs)
+
+testMakeFurigana = map (\(a,b) -> makeFurigana (KanjiPhrase a) (ReadingPhrase b))
+  [("いじり回す", "いじりまわす")
+  ,("弄りまわす", "いじりまわす")
+  , ("弄り回す", "いじりまわす")
+  , ("いじり回す", "いじりまわ") -- Fail
+  , ("窺う", "うかがう")
+  , ("黄色い", "きいろい")
+  , ("額が少ない", "がくがすくない")
+  -- , ("霞ヶ関", "かすみがせき")  -- Reading with no kanji
+  -- , ("霞ケ関", "かすみがせき")  -- Reading with no kanji
+  , ("ケント紙", "ケントし")
+  , ("二酸化ケイ素", "にさんかケイそ")
+  , ("ページ違反", "ぺーじいはん")
+  , ("シェリー酒", "シェリーしゅ")
+  ]
+
+-- isSameAs t1 t2
+--   | T.length t1 == T.length t2 = all compareChars (zip (T.unpack t1) (T.unpack t2))
+--   | otherwise = False
+
+-- isSameAsC c1 c2 = compareChars (c1, c2)
+
+-- compareChars = f
+--   where
+--     f ('ヶ', c2) = elem c2 ['か', 'が','ヶ', 'ケ']
+--     f ('ケ', c2) = elem c2 ['か', 'が','ヶ', 'ケ']
+--     f (c1, c2) = c1 == c2
 makeLenses ''SrsReviewStats
 makeLenses ''VocabDetails
 makeLenses ''KanjiDetails
