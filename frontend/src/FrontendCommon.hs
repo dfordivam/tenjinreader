80546bf18278fb9107b631cd09ea9743cbbcde20
Divam <dfordivam@gmail.com>
2017-12-15 20:32:27 +0900

    Edit srs item from text reader

--- frontend/src/FrontendCommon.hs
+++ frontend/src/FrontendCommon.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE NoImplicitPrelude #-}
@@ -30,6 +32,9 @@ import qualified Data.Text as T
 import Data.These as X
 import Data.Align as X
 
+import qualified Data.List.NonEmpty as NE
+import Data.List.NonEmpty (NonEmpty)
+
 --
 type AppMonadT t m = WithWebSocketT AppRequest t m
 type AppMonad t m = (MonadWidget t m)
@@ -47,6 +52,16 @@ handleVisibility v dv mv = elDynAttr "div" (f <$> dv) mv
 tshow :: (Show a) => a -> Text
 tshow = (T.pack . show)
 
+displayVocabT :: DomBuilder t m => Vocab -> m ()
+displayVocabT (Vocab ks) = do
+  let
+    f (Kana k) = text k
+    f (KanjiWithReading (Kanji k) r)
+      = el "ruby" $ do
+          text k
+          el "rt" $ text r
+  mapM_ f ks
+
 -- Controls to add/edit related srs items
 addEditSrsEntryWidget :: AppMonad t m
   => (Either KanjiId VocabId)
@@ -58,6 +73,7 @@ addEditSrsEntryWidget i t s = do
     widget s = case s of
       (Just sId) -> do
         ev <- button "Edit Srs Item"
+        openEditSrsItemWidget (sId <$ ev)
         return never
 
       (Nothing) -> do
@@ -70,12 +86,173 @@ addEditSrsEntryWidget i t s = do
 
   return ()
 
-displayVocabT :: DomBuilder t m => Vocab -> m ()
-displayVocabT (Vocab ks) = do
+openEditSrsItemWidget
+  :: (AppMonad t m)
+  => Event t (SrsEntryId)
+  -> AppMonadT t m ()
+openEditSrsItemWidget ev = do
+  srsItEv <- getWebSocketResponse $ GetSrsItem <$> ev
+
   let
-    f (Kana k) = text k
-    f (KanjiWithReading (Kanji k) r)
-      = el "ruby" $ do
-          text k
-          el "rt" $ text r
-  mapM_ f ks
+      modalWidget :: (AppMonad t m)
+        => Maybe (SrsEntryId, SrsEntry)
+        -> AppMonadT t m ()
+      modalWidget (Just s) = do
+        rec
+          d <- widgetHold (editWidget s)
+            ((return never) <$ switchPromptlyDyn d)
+        return ()
+
+      modalWidget Nothing = do
+        text $ "Some Error"
+
+      editWidget :: AppMonad t m
+        => (SrsEntryId, SrsEntry)
+        -> AppMonadT t m (Event t ())
+      editWidget (sId, s) = do
+        rec
+          (sNew, saveEv, closeEv) <- editWidgetView s ev
+          ev <- getWebSocketResponse $ EditSrsItem sId
+            <$> tagDyn sNew saveEv
+        return closeEv
+
+  void $ widgetHold (return ()) (modalWidget <$> srsItEv)
+
+modalDiv m = do
+  divClass "modal-backdrop fade in" $ return ()
+  elAttr "div" attr $ divClass "modal-dialog"
+    $ divClass "modal-content" m
+  where attr = ("class" =: "modal")
+          <> ("style" =: "display: block;")
+
+editWidgetView
+  :: MonadWidget t m
+  => SrsEntry
+  -> Event t ()
+  -> m (Dynamic t SrsEntry
+       , Event t (), Event t ())
+editWidgetView s savedEv = modalDiv $ do
+  divClass "modal-header" $ el "h3" $ do
+    text $ "Edit " <> (s ^. field . to (NE.head))
+
+  let bodyAttr = ("class" =: "modal-body")
+          <> ("style" =: "height: 400px;\
+              \overflow-y: scroll")
+      formAttr = ("class" =: "form-horizontal")
+        <> ("onsubmit" =: "return false;")
+      bodyForm m = elAttr "div" bodyAttr $
+        elAttr "form" formAttr m
+
+  ret <- bodyForm $ do
+
+    f <- divClass "form-group" $ do
+      elClass "label" "control-label col-sm-1" $
+        text "Field:"
+      editNonEmptyList (s ^. field) identity
+        $ \t x -> do
+             text t
+             e <- x
+             text ", "
+             return e
+
+    r <- divClass "form-group" $ do
+      elClass "label" "control-label col-sm-2" $
+        text "Reading:"
+      editNonEmptyList (s ^. readings) Reading
+        $ \t x -> do
+             text $ unReading t
+             e <- x
+             text ", "
+             return e
+
+    -- m <- elAttr "div" (("class" =: "form-group")
+    --       <> ("style" =: "height: 100px;\
+    --           \overflow-y: scroll")) $ do
+    m <- divClass "form-group" $ do
+      elClass "label" "control-label col-sm-2" $
+        text "Meanings:"
+      editNonEmptyList (s ^. meaning) Meaning
+        $ \t x -> do
+             text $ unMeaning t
+             e <- x
+             text ", "
+             return e
+
+    rn <- divClass "form-group" $ do
+      elClass "label" "control-label col-sm-2" $
+        text "Reading Notes:"
+      divClass "" $
+        textArea $ def &
+          textAreaConfig_initialValue .~
+            (maybe "" unReadingNotes
+             $ s ^. readingNotes)
+
+    mn <- divClass "form-group" $ do
+      elClass "label" "control-label col-sm-2" $
+        text "Meaning Notes:"
+      divClass "" $
+        textArea $ def &
+          textAreaConfig_initialValue .~
+            (maybe "" unMeaningNotes
+             $ s ^. meaningNotes)
+
+    let
+        g c v = gg c <$> value v
+        gg c t
+          | T.null t = Nothing
+          | otherwise = Just $ c t
+
+    return $ SrsEntry (_reviewState s)
+              <$> r <*> m
+              <*> (g ReadingNotes rn)
+              <*> (g MeaningNotes mn)
+              <*> f
+
+  divClass "modal-footer" $ do
+    let savedIcon = elClass "i" "" $ return ()
+    saveEv <- button "Save"
+    closeEv <- button "Close"
+    widgetHold (return ()) (savedIcon <$ savedEv)
+    return (ret, saveEv, closeEv)
+
+
+editNonEmptyList :: (_)
+  => NonEmpty v
+  -> (Text -> v)
+  -> (forall a . v -> m a -> m a)
+  -> m (Dynamic t (NonEmpty v))
+editNonEmptyList ne conT renderFun = do
+  let
+    rem = do
+      -- (e,_) <- elClass' "span" "glyphicon glyphicon-remove" $ return ()
+      (e,_) <- el' "a" $ text "(X)"
+      return $ domEvent Click e
+
+    initMap = Map.fromList $ zip [1..] (NE.toList ne)
+    showItem k t = do
+      ev <- renderFun t rem
+      return (t, (k,Nothing) <$ ev)
+
+  rec
+    let
+      remAddEv = Map.fromList . NE.toList <$> mergeList [addEv, remEv]
+      addEv = attachDyn newKeyDyn (tagDyn (Just . conT <$> value ti) enterPress)
+      remEv1 = switchPromptlyDyn $
+        (leftmost . (fmap snd) . Map.elems) <$> d
+      remEv = fmapMaybe g (attachDyn d remEv1)
+      g (m,e) = if Map.size m > 1
+        then Just e
+        else Nothing
+      newKeyDyn = ((+ 1) . fst . Map.findMax) <$> d
+
+      enterPress = ffilter (==13) (ti ^. textInput_keypress) -- 13 -> Enter
+      tiAttr = constDyn $ ("style" =: "width: 100%;")
+
+    ti <- divClass "col-sm-2" $ textInput $ def
+      & textInputConfig_attributes .~ tiAttr
+      & textInputConfig_setValue .~ ("" <$ enterPress)
+    d <- divClass "col-sm-6" $
+      elClass "p" "form-control-static" $
+        listHoldWithKey initMap remAddEv showItem
+
+  return $ (NE.fromList . (fmap fst) . Map.elems) <$> d
