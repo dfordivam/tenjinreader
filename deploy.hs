b4cd2acde8d0f7d5bbc4130780974a9cf7a5072e
Divam <dfordivam@gmail.com>
2018-02-20 15:08:01 +0900

    re-write from turtle to shelly

--- deploy.hs
+++ deploy.hs
@@ -1,15 +1,19 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i runghc -p 'ghc.withPackages (self: with self; [ turtle directory async text])'
+#! nix-shell -i runghc -p 'ghc.withPackages (self: with self; [ shelly ])'
 
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleContexts #-}
-import Turtle
-import System.Directory
-import Control.Concurrent.Async
-import Data.Text as T
-import Data.Text.IO as T
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 
-main = do
+import Shelly
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Data.Text (Text)
+import Prelude hiding (FilePath)
+import Data.Monoid ((<>))
+default (T.Text)
+
+main = shelly $ do
   -- let
   --   beBuildCmd = "nix-build -o backend-result -A ghc.backend"
   --   feBuildCmd = "nix-build -o frontend-result -A ghcjs.frontend"
@@ -20,8 +24,8 @@ main = do
   -- if not (be == ExitSuccess && fe == ExitSuccess)
   --   then putStrLn "Problem with nix-build"
   --   else do
-  --     bePath <- canonicalizePath "backend-result"
-  --     fePath <- canonicalizePath "frontend-result"
+  --     bePath <- realpath "backend-result"
+  --     fePath <- realpath "frontend-result"
   --     putStrLn bePath
   --     putStrLn fePath
 
@@ -32,36 +36,49 @@ main = do
 
 destHost = "tenjinreader.com"
 deployDir = "deploy-outputs"
+indexHtml = deployDir </> "index.html"
 
-exampleBePath :: Text
+exampleBePath :: FilePath
 exampleBePath = "/nix/store/kwvhlj6a0bvapm3rhgkhd737j5jxmy60-server-0.1.0"
-exampleFePath :: Text
+exampleFePath :: FilePath
 exampleFePath = "/nix/store/l91myjy2xq3054hb5sgrbml4xf7j8911-frontend-0.1.0.0"
 
+getHashFromPath :: FilePath -> Text
+getHashFromPath bePath = beHash
+  where
+    (Just beHash) = T.stripSuffix "-server-0.1.0"
+      =<< T.stripPrefix "/nix/store/" (toTextArg bePath)
+
+makeStaticDirContents :: FilePath -> FilePath -> Sh ()
 makeStaticDirContents bePath fePath = do
   let
-    (Just beHash) = T.stripSuffix "-server-0.1.0" =<< T.stripPrefix "/nix/store/" bePath
-    outJsFile = beHash <> ".js"
+    beHash = getHashFromPath bePath
+
+    outJsFile = (beHash) <.> "js"
 
     makeFeCode (s,d) = do
-      let dest = appDir <> "/" <> d
-          src = fePath <> "/" <> s
-          alljs = src <> "/all.js"
-          allextern = src <> "/all.js.externs"
-          outF = dest <> "/" <> outJsFile
-          cpCmd = "cp " <> indexHtml <> " " <> dest
-          ccCmd = closureCompilerPath <> "/bin/closure-compiler"
-            <> " --compilation_level ADVANCED "
-            <> " --js_output_file " <> outF
-            <> " --externs " <> allextern
-            <> " --js " <> alljs
-      T.putStrLn $ "Creating : '" <> outF <> "', from '" <> src <> "'."
-      T.putStrLn ccCmd
-      T.putStrLn cpCmd
+      let dest = appDir </> d
+          src = fePath </> s
+          alljs = src </> "all.js"
+          allextern = src </> "all.js.externs"
+          outF = dest </> outJsFile
+          ccCmdArgs :: [Text]
+          ccCmdArgs =
+            [ " --compilation_level ADVANCED "
+            , " --js_output_file " <> toTextArg outF
+            , " --externs " <> toTextArg allextern
+            , " --js " <> toTextArg alljs]
+
+      -- mkdir dest
+      trace $ "Creating : '" <> toTextArg outF <> "', from '" <> toTextArg src <> "'."
+
+      trace $ mconcat ccCmdArgs
+      -- shell ccCmd empty
+
+      -- cp indexHtml dest
 
     stDir = (deployDir  <> "/static")
     appDir = stDir <> "/app"
-    indexHtml = deployDir <> "/index.html"
 
   makeIndexHtml outJsFile indexHtml
   -- mkdir deployDir
@@ -70,21 +87,27 @@ makeStaticDirContents bePath fePath = do
   mapM_ makeFeCode feDestPaths
 
   let
-    lnCmd = "ln -s " <> bePath <> " " <> deployDir <> "/server"
-  T.putStrLn lnCmd
+    lnCmd = "ln -s " <> toTextArg bePath <> " " <> toTextArg deployDir <> "/server"
+  trace lnCmd
+  -- shell lnCmd empty
 
+doDeploy :: FilePath -> Sh ()
 doDeploy bePath = do
   let
-    (Just beHash) = T.stripSuffix "-server-0.1.0" =<< T.stripPrefix "/nix/store/" bePath
-    ccCmd = "nix-copy-closure --sign --include-outputs " <> destHost <> " " <> bePath
+    beHash = getHashFromPath bePath
+    ccCmd = "nix-copy-closure --sign --include-outputs " <> toTextArg destHost <> " " <> toTextArg bePath
     targetDir = destHost <> ":~/" <> beHash
-    scpCmd = "scp -r " <> deployDir <> " " <> targetDir
-  T.putStrLn ccCmd
-  T.putStrLn scpCmd
+    scpCmd = "scp -r " <> toTextArg deployDir <> " " <> toTextArg targetDir
+  trace ccCmd
+  -- shell ccCmd empty
 
-closureCompilerPath = "/nix/store/n7h36mmdgd9y5g59qasznjvsl3psg0g2-closure-compiler-20170218"
+  trace scpCmd
+  -- shell scpCmd empty
 
-feDestPaths :: [(Text,Text)]
+closureCompilerPath :: FilePath
+closureCompilerPath = "/nix/store/n7h36mmdgd9y5g59qasznjvsl3psg0g2-closure-compiler-20170218/bin/closure-compiler"
+
+feDestPaths :: [(FilePath, FilePath)]
 feDestPaths =
   [ ("bin/frontend.jsexe"
    , "complete-norecog")
@@ -98,5 +121,7 @@ feDestPaths =
    , "srsonly")]
 
 makeIndexHtml jsName outF = do
-  let sedCmd = "sed \"s@all_cc_js_name@" <> jsName <> "@\" index.html.template > " <> outF
-  T.putStrLn $ "Executing: " <> sedCmd
+  let sedCmd = "sed \"s@all_cc_js_name@" <>
+        (toTextArg jsName) <> "@\" index.html.template > " <> toTextArg outF
+  trace $ "Executing: " <> sedCmd
+  -- shell sedCmd empty
