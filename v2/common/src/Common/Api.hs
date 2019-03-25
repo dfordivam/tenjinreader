b6b1081a4268967ced4fbaa46aba7894f6993562
Divam <dfordivam@gmail.com>
2019-03-25 18:41:20 +0900

    Serve a simple servant api

--- v2/common/src/Common/Api.hs
+++ v2/common/src/Common/Api.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 module Common.Api where
+
+import Servant
+import Data.Text
+import Data.Proxy
+
+type ReaderAPI =
+  "api" :> "book" :> Capture "bookid" Int :> Get '[JSON] Text
+  :<|> "api" :> "book" :> Capture "bookid" Int :> Delete '[JSON] ()
+
+readerAPI :: Proxy ReaderAPI
+readerAPI = Proxy
 
 commonStuff :: String
 commonStuff = "Here is a string defined in code common to the frontend and backend."
