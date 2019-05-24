1bd892962bd9b605ea6c4ac276779c844f67a5b6
Divam <dfordivam@gmail.com>
2019-05-24 19:33:38 +0900

    Home page: random sentence widget

--- v2/frontend/src/Frontend/Home.hs
+++ v2/frontend/src/Frontend/Home.hs
@@ -20,6 +20,7 @@ import Data.Functor.Identity
 
 import Common.Api
 import Common.Route
+import Frontend.Common
 import Obelisk.Generated.Static
 
 home
@@ -33,16 +34,11 @@ home
      )
   => m ()
 home = do
-  text "HOME"
   divClass "tile is-ancestor" $ do
     divClass "tile is-vertical" $ do
       divClass "tile" $ do
         divClass "tile is-parent is-vertical" $ do
-          elClass "article" "tile is-child notification is-primary" $ do
-            elClass "p" "title" $ do
-              text "Random sentence"
-            elClass "p" "subtitle" $ do
-              text "******"
+          elClass "article" "tile is-child" $ randomSentenceWidget
       divClass "tile is-parent" $ do
         elClass "article" "tile is-child notification is-danger is-8" $ do
           elClass "p" "title" $ do
@@ -59,3 +55,28 @@ home = do
             divClass "content" $ return ()
   e <- button "click"
   count e >>= display
+
+randomSentenceWidget
+  :: ( DomBuilder t m
+     , Routed t (R FrontendRoute) m
+     , PostBuild t m
+     , MonadFix m
+     , MonadHold t m
+     , SetRoute t (R FrontendRoute) m
+     , RouteToUrl (R FrontendRoute) m
+     )
+  => m ()
+randomSentenceWidget = divClass "box" $ do
+  let
+    s = "ここからそのホテルまでどのくらいの距離ですか。"
+    e = "How far is it from here to the hotel?"
+  elClass "p" "is-size-2-desktop is-size-3-touch" $ do
+    text s
+  elClass "nav" "level" $ do
+    divClass "level-left" $ do
+      elClass "p" "is-size-4-desktop is-size-5-touch" $ text e
+    divClass "level-right" $ do
+      btnIcon "" "fa-plus"
+      btnIcon "" "fa-check-double"
+      btnIcon "" "fa-align-justify"
+  blank
