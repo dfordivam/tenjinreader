2cebdbe5bb2d93360f2e3fd087a92dabfc8c4668
Divam <dfordivam@gmail.com>
2019-05-24 21:32:28 +0900

    home page: Word list

--- v2/frontend/src/Frontend/Home.hs
+++ v2/frontend/src/Frontend/Home.hs
@@ -13,10 +13,12 @@ import Obelisk.Route
 import Obelisk.Route.Frontend
 import Reflex.Dom.Core
 
-import qualified Data.Text as T
-import Data.Dependent.Sum (DSum(..))
+import Control.Monad
 import Control.Monad.Fix
 import Data.Functor.Identity
+import Data.Traversable
+import qualified Data.Text as T
+import Data.Dependent.Sum (DSum(..))
 
 import Common.Api
 import Common.Route
@@ -38,7 +40,7 @@ home = do
     divClass "tile is-vertical" $ do
       divClass "tile" $ do
         divClass "tile is-parent is-vertical" $ do
-          elClass "article" "tile is-child" $ randomSentenceWidget
+          elClass "article" "tile is-child" $ divClass "box" $ randomSentenceWidget
       divClass "tile is-parent" $ do
         elClass "article" "tile is-child notification is-danger is-8" $ do
           elClass "p" "title" $ do
@@ -46,13 +48,7 @@ home = do
           elClass "p" "subtitle" $ do
             text "Book 1"
           divClass "content" $ return ()
-        elClass "article" "tile is-child notification is-success" $ do
-          divClass "content" $ do
-            elClass "p" "title" $ do
-              text "Words list"
-            elClass "p" "subtitle" $ do
-              text "long list"
-            divClass "content" $ return ()
+        elClass "article" "tile is-child" $ divClass "box" $ recentWordList
   e <- button "click"
   count e >>= display
 
@@ -66,7 +62,7 @@ randomSentenceWidget
      , RouteToUrl (R FrontendRoute) m
      )
   => m ()
-randomSentenceWidget = divClass "box" $ do
+randomSentenceWidget = do
   let
     s = "ここからそのホテルまでどのくらいの距離ですか。"
     e = "How far is it from here to the hotel?"
@@ -81,3 +77,33 @@ randomSentenceWidget = divClass "box" $ do
   widgetHold blank $ ffor showEng $ \_ ->
     elClass "p" "is-size-4-desktop is-size-5-touch" $ text e
   blank
+
+recentWordList
+  :: ( DomBuilder t m
+     , Routed t (R FrontendRoute) m
+     , PostBuild t m
+     , MonadFix m
+     , MonadHold t m
+     , SetRoute t (R FrontendRoute) m
+     , RouteToUrl (R FrontendRoute) m
+     )
+  => m ()
+recentWordList = do
+  let
+    words =
+      [ ("距離", "きょり")
+      , ("途中", "とちゅう")
+      , ("恐ろしい", "おそろしい")
+      , ("足元", "あしもと")
+      , ("見事", "みごと")
+      , ("複雑性", "ふくざつせい")
+      ]
+  forM_ words $ \(k, r) -> divClass "card" $ do
+    (e,_) <- elClass' "div" "card-header" $ do
+      elClass "p" "card-header-title" $ elClass "span" "is-size-4-desktop is-size-5-touch" $ text k
+      elClass' "a" "card-header-icon" $ elClass "span" "icon" $
+        elClass "i" "fas fa-angle-down" blank
+    let openEv = domEvent Click e
+    widgetHold blank $ ffor openEv $ \_ -> do
+      divClass "card-content" $ do
+        divClass "content" $ text "彼《かれ》は現代生活《げんだいせいかつ》の複雑性《ふくざつせい》について長々《ながなが》と話《はな》した。"
