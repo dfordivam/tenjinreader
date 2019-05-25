353702b5686cb03cdb108a4d1a3b49d4563abd30
Divam <dfordivam@gmail.com>
2019-05-25 15:04:21 +0900

    use hero fullheight

--- v2/frontend/src/Frontend.hs
+++ v2/frontend/src/Frontend.hs
@@ -38,7 +38,7 @@ frontend = Frontend
   { _frontend_head = headEl
   , _frontend_body = do
       rec (navDyn, rc) <- nav click
-          click <- divClass "columns" $ do
+          click <- divClass "hero is-fullheight columns" $ do
             elDynClass "div" ((<>) "column is-narrow " <$> (ffor navDyn $ bool "is-hidden" "")) $ sidePanel navDyn
             mainContainer (sections rc)
       return ()
