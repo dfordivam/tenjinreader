4802b768e1dbbf7487c8351a71de1462055bb318
Divam <dfordivam@gmail.com>
2019-05-28 16:59:44 +0900

    word meaning modal UI

--- v2/frontend/src/Frontend/Modals.hs
+++ v2/frontend/src/Frontend/Modals.hs
@@ -72,17 +72,13 @@ wordMeanings = void $ prerender (return ()) $ divClass "" $ do
           return (y,h)
       (e1,_) <- elAttr' "div" attrBack $ return ()
       elAttr "div" (attrFront y h) $
-        divClass "" $
-          elAttr "div" (("class" =: "notification")
-            <> ("style" =: "max-height: 30vh;\
-                           \overflow-y: auto;\
-                           \overflow-x: hidden;\
-                           \padding: 2vw;")) $ do
-            (e2,_) <- elClass' "button" "delete" $ return ()
-            _ <- m
-            return $ leftmost
-              [domEvent Click e2
-              , domEvent Click e1]
+        elAttr "div" (("class" =: "notification")
+          <> ("style" =: "max-height: 30vh; overflow-y: auto; overflow-x: hidden; min-width: 96vw;")) $ do
+          (e2,_) <- elClass' "button" "delete" $ return ()
+          _ <- m
+          return $ leftmost
+            [domEvent Click e2
+            , domEvent Click e1]
 
     wd :: (Monad m1, DOM.MonadJSM m1, DomBuilder t m1, DOM.IsElement e)
       => Maybe (Maybe e, [VocabDetails])
@@ -93,7 +89,7 @@ wordMeanings = void $ prerender (return ()) $ divClass "" $ do
 
   openEv <- getPostBuild
   (e, _) <- el' "div" blank
-  let details = (Just $ _element_raw e, [("word", "meaning")])
+  let details = (Just $ _element_raw e, vocabs)
       detailsEv = details <$ openEv
   rec
     let ev = leftmost [Just <$> detailsEv
@@ -103,13 +99,27 @@ wordMeanings = void $ prerender (return ()) $ divClass "" $ do
 
   return ()
 
-type VocabDetails = (Text, Text)
+type VocabDetails = (Text, Text, Text)
+
+vocabs :: [VocabDetails]
+vocabs =
+  [ ("長長", "Long, Drawn-Out, Very long", "Adv")
+  , ("ながなが", "Long, Drawn-Out, Very long", "Adv")
+  ]
 
 showVocabDetails
   :: forall t m .( DomBuilder t m
      )
   => VocabDetails
   -> m ()
-showVocabDetails (v, m) = divClass "" $ do
-  divClass "title" $ text v
-  divClass "" $ text m
+showVocabDetails (v, m, p) = divClass "message" $ do
+  void $ divClass "message-body" $ do
+    divClass "level is-mobile" $ do
+      divClass "level-left" $ divClass "level-item is-size-3" $ text v
+      divClass "level-right" $ divClass "level-item" $ do
+        btnIcon "" "fa-plus"
+        btnIcon "" "fa-check"
+        btnIcon "" "fa-align-justify"
+    divClass "" $ do
+      elClass "span" "" $ text $ "(" <> p <> ") "
+      elClass "span" "is-size-5" $ text m
